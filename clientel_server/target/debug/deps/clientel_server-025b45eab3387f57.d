f6b67216c6d928d3048e7abb120ab70931a98e71
Silver Luhtoja <50075719+SilverLuhtoja@users.noreply.github.com>
2023-01-11 23:25:04 +0200

    Delete clientel_server/target directory

--- clientel_server/target/debug/deps/clientel_server-025b45eab3387f57.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Silver Luhtoja\Desktop\UDP_server\clientel_server\target\debug\deps\clientel_server-025b45eab3387f57.rmeta: src\main.rs
-
-c:\Users\Silver Luhtoja\Desktop\UDP_server\clientel_server\target\debug\deps\clientel_server-025b45eab3387f57.d: src\main.rs
-
-src\main.rs:
--- clientel_server/target/debug/deps/clientel_server-7bccca7ca8801bba.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Silver Luhtoja\Desktop\UDP_server\clientel_server\target\debug\deps\clientel_server-7bccca7ca8801bba.rmeta: src\main.rs
-
-c:\Users\Silver Luhtoja\Desktop\UDP_server\clientel_server\target\debug\deps\clientel_server-7bccca7ca8801bba.d: src\main.rs
-
-src\main.rs:
