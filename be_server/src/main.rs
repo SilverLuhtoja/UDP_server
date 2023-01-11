b02c1d0d72c91c1e6d7be1e42547b759c0f563c5
Silver Luhtoja <silver.luhtoja@gmail.com>
2023-01-11 23:21:55 +0200

    refactoring some things

--- be_server/src/main.rs
+++ be_server/src/main.rs
@@ -1,26 +1,31 @@
 use std::net::UdpSocket;
 
+const ADDR:&str = "127.0.0.1";
+const PORT:u16 = 4242;
+
 fn main() -> std::io::Result<()> {
-    const PORT:u16 = 4242;
-    let addr = format!("127.0.0.1:{}", PORT);
-    let socket = UdpSocket::bind(addr)?; // for UDP4
+    // for UDP4
     // let socket = UdpSocket::bind("[::]:2000")?;  // for UDP4/6
+    let socket = UdpSocket::bind(format!("{}:{}",ADDR, PORT))?; 
     let mut buf = [0; 2048];
 
     println!("Creating server : {:?}.Listening....", socket);
+    println!("Waiting messages:");
     loop {
         println!();
-        println!("Waiting messages:");
-        // receiving from connected server
-        let (amt, src) = socket.recv_from(&mut buf)?;
-        println!("amt : {} , src : {}", amt,src);
-        let filled_buf = &mut buf[..amt];
-        println!("message: {:?}", String::from_utf8_lossy(filled_buf));
+        // (bite_slice, address where it came from)
+        let (amt, src) = socket.recv_from(&mut buf).expect("incoming message failed");
+        let incoming_message = String::from_utf8_lossy(&mut buf[..amt]);
+        println!("client <{}>: {:?}", src, incoming_message);
         
-        // Redeclare `buf` as slice of the received data
-	    // and send data back to origin.
-        let buf = &mut buf[..amt];
-        let messagef= ["Server: ".as_bytes(),buf].concat();
-        socket.send_to(&messagef, &src)?;
+        if incoming_message == "connect" {
+            let message  = format!("Successful connection with {}:{}",ADDR,PORT);
+            socket.send_to(message.as_bytes(), &src)?;
+        }else{
+            // Redeclare `buf` as slice of the received data
+            // and send data back to origin.
+            let buf = &mut buf[..amt];
+            socket.send_to(&buf, &src)?;
+        }
     }
 }
