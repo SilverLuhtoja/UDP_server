f295fe244d8d3937f7407c7d38075ac0ee238794
Anna <anna.lazarenkova@gmail.com>
2023-01-16 16:51:57 +0200

    very basic visualizer

--- clientel_server/src/lib.rs
+++ clientel_server/src/lib.rs
@@ -1,6 +1,150 @@
+use macroquad::prelude::*;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
+
+const CAP_SIZE: f32 = 1.0;
+const PADDING: f32 = 20.0;
+
+
+#[derive(Debug, Clone)]
+pub struct GameBoard {
+    pub board: Vec<Vec<i32>>,
+    pub setup: Settings,
+}
+
+#[derive(Debug, Clone)]
+pub struct Settings {
+    pub center_x_offset: f32,
+    pub center_y_offset: f32,
+    pub cube_size: f32,
+    pub board_height: f32,
+}
+
+impl Settings{
+    pub fn new(board: Vec<Vec<i32>>)-> Self{
+        let horizontal_cubes = board[0].len() as f32;
+        let cube_size = get_cube_size(horizontal_cubes);
+
+        Self { 
+            center_x_offset: get_center_x(horizontal_cubes, cube_size),
+            center_y_offset: get_center_y(board.len() as f32, cube_size),
+            cube_size,
+            board_height: horizontal_cubes,
+        }
+    }
+}
+
+impl GameBoard {
+    pub fn new(map: Vec<Vec<i32>>) -> Self {
+        let setup = Settings::new(map.clone());
+        Self {
+            board: map.clone(),
+            setup
+        }
+    }
+
+    pub fn draw(&self) {
+        draw_rectangle(
+            20.0,
+            self.setup.center_y_offset + self.setup.center_y_offset - CAP_SIZE - 20.0,
+            (self.board[0].len() as f32 * self.setup.cube_size) + CAP_SIZE,
+            (self.board.len() as f32 * self.setup.cube_size) + CAP_SIZE,
+            WHITE,
+        );
+        for i in 0..self.board.len() {
+            for j in 0..self.board[0].len() {
+                draw_rectangle(
+                    (j as f32 * self.setup.cube_size) + 20.0,
+                    (i as f32 * self.setup.cube_size) + self.setup.center_y_offset + self.setup.center_y_offset - 20.0,
+                    self.setup.cube_size - CAP_SIZE,
+                    self.setup.cube_size - CAP_SIZE,
+                    self.match_color(self.board[i][j]),
+                );
+            }
+        }
+    }
+
+    fn match_color(&self, ch: i32) -> Color {
+        match ch {
+            1  => GRAY,
+            0  => WHITE,
+            _ => RED,
+        }
+    }
+}
+
+fn get_center_x(horizontal_cubes: f32, cube_size: f32) -> f32 {
+    return (screen_width() - (horizontal_cubes as f32 * cube_size)) /2.0;
+}
+
+fn get_center_y(lines: f32, cube_size: f32) -> f32 {
+    return (screen_height() - (lines as f32 * cube_size)) /2.0;
+}
+
+fn get_cube_size(horizontal_cubes: f32) -> f32 {
+    if horizontal_cubes <= 10.0 {return 20.0} 
+    if horizontal_cubes <= 20.0 {return 10.0} 
+    if horizontal_cubes <= 30.0 {return 5.0} 
+    return 10.0
+}
+
+
+#[derive(Debug, Clone)]
+pub struct MazeVisual {
+    pub width: f32,
+    pub height: f32,
+}
+
+impl MazeVisual {
+    pub fn new(game_board: GameBoard) -> Self {
+        Self {
+            width: screen_width() - PADDING * 2.0,
+            height: screen_height() - PADDING * 3.0 - (game_board.board.len() as f32 * game_board.setup.cube_size) + CAP_SIZE,
+        }
+    }
+    pub fn draw(&self) {
+        draw_rectangle(
+            PADDING,
+            PADDING,
+            self.width,
+            self.height,
+            WHITE,
+        );
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ScoreBoard {
+    pub width: f32,
+    pub height: f32,
+    pub text_address_x: f32,
+    pub text_address_y: f32,
+}
+
+impl ScoreBoard {
+    pub fn new(visual_box: MazeVisual) -> Self {
+        Self {
+            width: screen_width() / 2.0 - PADDING * 5.0 ,
+            height: screen_height() - PADDING * 3.0 - visual_box.height,
+            text_address_x: screen_width() / 2.0,
+            text_address_y: PADDING * 3.0 + visual_box.height,
+        }
+    }
+    pub fn draw(&self) {
+        draw_text("SCORE:", self.text_address_x, self.text_address_y, 20.0, RED);
+        draw_rectangle(
+            screen_width() / 2.0 + PADDING * 4.0,
+            screen_height() - self.height - PADDING,
+            self.width,
+            self.height,
+            YELLOW,
+        );
+    }
+}
+
+
 
 #[cfg(test)]
 mod tests {
--- clientel_server/src/main.rs
+++ clientel_server/src/main.rs
@@ -1,26 +1,90 @@
 use std::io::{self, BufRead};
 use std::net::UdpSocket;
 use std::str;
+use macroquad::prelude::*;
+use clientel_server::*;
 
 const ADDR:&str = "127.0.0.1";
 const SERVER_PORT:u16 = 4242;
 const CLIENT_PORT:u16 = 34254;
 
-fn main() -> std::io::Result<()> {
+// static map: Vec<u8>;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "MAZE WARS".to_owned(),
+        window_width: 1200,
+        window_height: 800,
+        window_resizable: true,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() -> std::io::Result<()> {
     let socket = UdpSocket::bind(format!("{}:{}",ADDR, CLIENT_PORT))?;
     println!("Establishing UDP socket address at : {:?}", socket);
     try_to_connect(&socket); // "fake handshake"
+    let map: Vec<Vec<i32>> = vec![
+        vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        vec![1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        vec![1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1],
+        vec![1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        vec![1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1],
+        vec![1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1],
+        vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ];
+    loop{
+        clear_background(Color::new(0.0, 0.0, 0.0, 0.8));
 
-    println!("\nwrite to server:");
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        let line = line.unwrap();
-        if &line == "BYE" {
-            break;
+        let game_board = GameBoard::new(map.clone());
+        let visual_board = MazeVisual::new(game_board.clone());
+        let score_board = ScoreBoard::new(visual_board.clone());
+        game_board.draw();
+        visual_board.draw();
+        score_board.draw();
+        
+
+        if is_key_pressed(KeyCode::Left) {
+            println!("\nwrite to server:");
+            socket.send("Left".as_bytes()).expect("Error on send");
         }
-        socket.send(line.as_bytes()).expect("Error on send");
-        read_incoming_messages(&socket);
+
+        if is_key_pressed(KeyCode::Right) {
+            println!("\nwrite to server:");
+            socket.send("Right".as_bytes()).expect("Error on send");
+        }
+
+        if is_key_pressed(KeyCode::Up) {
+            println!("\nwrite to server:");
+            socket.send("Up".as_bytes()).expect("Error on send");
+        }
+
+        if is_key_pressed(KeyCode::Down) {
+            println!("\nwrite to server:");
+            socket.send("Down".as_bytes()).expect("Error on send");
+        }
+
+        if is_key_pressed(KeyCode::Space) {
+            println!("\nwrite to server:");
+            socket.send("Shoot".as_bytes()).expect("Error on send");
+        }
+
+        // let stdin = io::stdin();
+        // for line in stdin.lock().lines() {
+        //     let line = line.unwrap();
+        //     if &line == "BYE" || &line == "bye" {
+        //         socket.send("stop".as_bytes()).expect("Error on send");
+        //         break;
+        //     }
+        //     socket.send(line.as_bytes()).expect("Error on send");
+        //     read_incoming_messages(&socket);
+        // }
+
+        next_frame().await
     }
+
+    
     Ok(())
 }
 
@@ -38,5 +102,19 @@ fn read_incoming_messages(socket :&UdpSocket) {
 	let (amt, _src) = socket.recv_from(&mut buf).expect("incoming message failed");
     let filled_buf = &mut buf[..amt];
     let incoming_message = String::from_utf8_lossy(filled_buf).into_owned();
+
     println!("SERVER --> {:?}", incoming_message);
+    if incoming_message.contains(""){
+
+    }
 }
+
+
+
+// fn draw_the_score(){
+//     draw_text("SCORE:", 620.0, 700.0, 20.0, WHITE);
+//     draw_line(700.0, 620.0, 1180.0, 620.0, 1.0, RED); //top
+//     draw_line(700.0, 780.0, 1180.0, 780.0, 1.0, RED); //bottom
+//     draw_line(700.0, 620.0, 700.0, 780.0, 1.0, RED); //left
+//     draw_line(1180.0, 620.0, 1180.0, 780.0, 1.0, RED); //right
+// }
