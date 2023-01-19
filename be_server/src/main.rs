d260bd9ac6dc0757d040e5ccd21e3bf841b43892
Silver Luhtoja <silver.luhtoja@gmail.com>
2023-01-19 23:51:50 +0200

    I dont know:

--- be_server/src/main.rs
+++ be_server/src/main.rs
@@ -1,6 +1,8 @@
 mod map;
 mod maze;
+mod player;
 
+use player::{Player, Point};
 use serde::{Deserialize, Serialize};
 use serde_json::{json};
 use serde_json::Value as JsonValue;
@@ -11,11 +13,6 @@ use std::collections::HashMap;
 const ADDR: &str = "127.0.0.1";
 const PORT: u16 = 4242;
 
-#[derive(Clone, Copy, Debug, PartialEq, Deserialize, Serialize)]
-pub struct Point {
-    pub x: f32,
-    pub y: f32,
-}
 
 #[derive(Clone, Debug, PartialEq,Deserialize, Serialize)]
 struct Data{
@@ -26,17 +23,21 @@ struct Data{
 #[derive(Clone, Debug, PartialEq,Deserialize, Serialize)]
 struct BroadcastMessage{
     map: JsonValue,
-    location: Point,
+    players : HashMap<SocketAddr, Player>
 }
-
 fn main() -> std::io::Result<()> {
     let socket = UdpSocket::bind(format!("{}:{}", ADDR, PORT))?;
     let mut buf = [0; 24000];
     let mut grid = Grid::new(10, 10, LOW);
     grid.generate_maze();
     let map = grid.convert_to_map();
-    let mut connected_players:HashMap<SocketAddr, BroadcastMessage> = HashMap::new();
+    let mut players:HashMap<SocketAddr, Player> = HashMap::new();
     println!("Creating server : {:?}.Listening....", socket);
+
+    let mut message = BroadcastMessage{
+                map: json!(map),
+                players : players.clone()
+            };
 
     loop {
         println!();
@@ -46,38 +47,23 @@ fn main() -> std::io::Result<()> {
         println!("client <{}>: {:?}", src, incoming_message);
 
         let data: Data = serde_json::from_str(&incoming_message)?;
-        
         if data.message_type == "movement" {
-            let current_player = connected_players.get_mut(&src).expect("ADD PLAYER < NOT IN HASH >");
+            let current_player = players.get_mut(&src).expect("ADD PLAYER < NOT IN HASH >");
             let point:Point = serde_json::from_value(data.data)?;
             current_player.location = point;
            
         }
         if data.message_type == "connect" {
-            // let message = json!({
-            // "map" : map,
-            // "location" : player
-            // });
-            let multiplier = (connected_players.len() + 1) as f32;
+            // different location for each connecting player
+            let multiplier = (players.len() + 1) as f32;
             let location = Point { x: 100.0 * multiplier, y: 100.0 * multiplier};
-            let message = BroadcastMessage{
-                map: json!(map),
-                location
-            };
-
-
-            connected_players.insert(src, message);
-            // socket
-            //     .connect(&src)
-            //     .expect("SERVER: connect function failed");
-            // socket.send(json!(&message).to_string().as_bytes())?;
-
-           
+            let player = Player::new(location);
+            players.insert(src,player);
         }
 
-
-        for (addr,v) in &connected_players{
-            socket.send_to(json!(&v).to_string().as_bytes(), addr)?;
+        message.players = players.clone();
+        for (addr,_) in &players{
+            socket.send_to(json!(&message).to_string().as_bytes(), addr)?;
         }
     }
 }
