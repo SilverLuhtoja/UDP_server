e94734efbd493d520c9f42f888003d351e962f0d
Silver Luhtoja <silver.luhtoja@gmail.com>
2023-01-19 23:40:19 +0200

    not good

--- mazeawar/src/main.rs
+++ mazeawar/src/main.rs
@@ -1,8 +1,6 @@
 use crate::client_server::*;
 use crate::map::Map;
-use crate::maze::{Grid, LOW};
 use crate::player::*;
-use local_ip_address::local_ip;
 use macroquad::prelude::*;
 use serde::{Deserialize, Serialize};
 use serde_json::Value as JsonValue;
@@ -21,11 +19,12 @@ struct Data {
     map: Map,
     players: HashMap<SocketAddr, Player>,
 }
-// #[derive(Serialize, Deserialize)]
-// struct Data {
-//     map: Map,
-//     location: Point,
-// }
+
+#[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
+struct Message {
+    message_type: String,
+    data: JsonValue,
+}
 
 fn window_conf() -> Conf {
     Conf {
@@ -36,12 +35,6 @@ fn window_conf() -> Conf {
     }
 }
 
-#[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
-struct Message {
-    message_type: String,
-    data: JsonValue,
-}
-
 // const ADDR: &str = "127.0.0.1";
 const ADDR: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
 const TIMER: u8 = 60;
@@ -49,14 +42,6 @@ const TIMER: u8 = 60;
 #[macroquad::main(window_conf)]
 async fn main() -> std::io::Result<()> {
     let server_addr: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4242);
-
-    // let spawn_point = Point::new(20.0, 20.0);
-    // let mut player = Player::new(spawn_point);
-    // let spawn_point2 = Point::new(50.0, 50.0);
-    // let mut player2 = Player::new(spawn_point2);
-
-    let mut players: HashMap<SocketAddr, Player> = HashMap::new();
- 
     let client = Client::new(ADDR);
     let my_addr = "127.0.0.1:34254";
     client.socket.connect(server_addr).expect("Connecting With Server Failed!");
@@ -70,10 +55,10 @@ async fn main() -> std::io::Result<()> {
 
     let data: Data = serde_json::from_str(&read_incoming_messages(&client.socket))?;
     let map = data.map;
-    players = data.players;
-    let mut  my_point = Point::new(0.0,0.0);
-    // player.set_postion(data.location);
+    let mut players: HashMap<SocketAddr, Player> = data.players;
+    let mut my_point = Point::new(0.0,0.0);
     let mut fps = TIMER;
+
     loop {
         map.draw();
         // allPlayers update (including player itself)
@@ -83,18 +68,17 @@ async fn main() -> std::io::Result<()> {
             }
             player.draw();
         }
-        // player.draw();
-        println!("{}", fps);
-        fps -= 1;
-        if fps == 0 {
-            println!("Sending Update now");
-            message = Message {message_type: "update".to_string(),data: json!(""),};
-            client.socket.send(serde_json::to_string(&message)?.as_bytes())?;
 
-            let data: Data = serde_json::from_str(&read_incoming_messages(&client.socket))?;
-            players = data.players;
-            fps=TIMER;
-        }
+
+        //-- Request update - -//
+        // fps -= 1;
+        // if fps == 0 {
+        //     message = Message {message_type: "update".to_string(),data: json!(""),};
+        //     client.socket.send(serde_json::to_string(&message)?.as_bytes())?;
+        //     let data: Data = serde_json::from_str(&read_incoming_messages(&client.socket))?;
+        //     players = data.players;
+        //     fps=TIMER;
+        // }
 
 
 
@@ -103,32 +87,32 @@ async fn main() -> std::io::Result<()> {
             message = Message {message_type: "movement".to_string(),data: json!(point),};
             client.socket.send(serde_json::to_string(&message)?.as_bytes())?;
 
-            // data = serde_json::from_str(&read_incoming_messages(&client.socket))?;
-            // player.set_postion(data.location);
+            let data: Data = serde_json::from_str(&read_incoming_messages(&client.socket))?;
+            players = data.players;
         }
         if is_key_pressed(KeyCode::D) {
             let point = Point::new(my_point.x + 20.0, my_point.y);
             message = Message {message_type: "movement".to_string(),data: json!(point),};
             client.socket.send(serde_json::to_string(&message)?.as_bytes())?;
 
-            // data = serde_json::from_str(&read_incoming_messages(&client.socket))?;
-            // player.set_postion(data.location);
+            let data: Data = serde_json::from_str(&read_incoming_messages(&client.socket))?;
+            players = data.players;
         }
         if is_key_pressed(KeyCode::W) {
             let point = Point::new(my_point.x, my_point.y - 20.0);
             message = Message {message_type: "movement".to_string(),data: json!(point),};
             client.socket.send(serde_json::to_string(&message)?.as_bytes())?;
 
-            // data = serde_json::from_str(&read_incoming_messages(&client.socket))?;
-            // player.set_postion(data.location);
+            let data: Data = serde_json::from_str(&read_incoming_messages(&client.socket))?;
+            players = data.players;
         }
         if is_key_pressed(KeyCode::S) {
             let point = Point::new(my_point.x, my_point.y + 20.0);
             message = Message {message_type: "movement".to_string(),data: json!(point),};
             client.socket.send(serde_json::to_string(&message)?.as_bytes())?;
 
-            // data = serde_json::from_str(&read_incoming_messages(&client.socket))?;
-            // player.set_postion(data.location);
+            let data: Data = serde_json::from_str(&read_incoming_messages(&client.socket))?;
+            players = data.players;
         }
 
         if is_key_pressed(KeyCode::Escape) {
